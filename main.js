e7e5f96aea50d42ded971632a888264bca5e3934
Anshul Gupta <66676986+AnshuL-g17@users.noreply.github.com>
2026-05-06 02:50:36 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,108 +1,104 @@
 window.addEventListener('load', init);
 
-
+// Levels based on score
 const levels = {
-	easy: 6,
-	medium: 4,
-	hard: 2
-}
+  easy: { time: 6, words: [] },
+  medium: { time: 4, words: [] },
+  hard: { time: 3, words: [] },
+  insane: { time: 2, words: [] }
+};
 
-const currentLevel = levels.easy;
 let time = 6;
 let score = 0;
-let isPlaying;
+let isPlaying = false;
+let currentLevel = "easy";
 
+// DOM elements
 const wordInput = document.querySelector('#word-input');
 const currentWord = document.querySelector('#current-word');
 const scoreDisplay = document.querySelector('#score');
 const timeDisplay = document.querySelector('#time');
 const message = document.querySelector('#message');
 const seconds = document.querySelector('#seconds');
-var lower = wordInput.value.toLowerCase();
 
+// Word banks
+levels.easy.words = [
+  'hat','cat','dog','run','sun','book','pen','cup','tree','ball'
+];
 
-const words = [
-  'hat',
-  'river',
-  'lucky',
-  'statue',
-  'generate',
-  'stubborn',
-  'cocktail',
-  'runaway',
-  'joke',
-  'developer',
-  'establishment',
-  'hero',
-  'javascript',
-  'nutrition',
-  'revolver',
-  'echo',
-  'siblings',
-  'investigate',
-  'horrendous',
-  'symptom',
-  'laughter',
-  'magic',
-  'master',
-  'space',
-  'definition'
+levels.medium.words = [
+  'river','lucky','hero','magic','space','master','echo','joke','happy','smart'
+];
+
+levels.hard.words = [
+  'developer','javascript','nutrition','revolver','siblings','investigate','symptom'
+];
+
+levels.insane.words = [
+  'establishment','horrendous','extraordinary','communication','transformation','responsibility'
 ];
 
 function init() {
-	  seconds.innerHTML = currentLevel;
-	  showWord(words);
-	  wordInput.addEventListener('input', startMatch);
-	  setInterval(countdown, 1000);
-	  setInterval(checkStatus, 50);
+  updateLevel();
+  showWord();
+  wordInput.addEventListener('input', startMatch);
+  setInterval(countdown, 1000);
+  setInterval(checkStatus, 50);
 }
 
-function startMatch(){
-	if (matchWords()) {
-		isPlaying = true;
-		time = currentLevel + 1;
-		showWord(words);
-		wordInput.value='';
-		score++;
-	}
+function updateLevel() {
+  if (score >= 30) currentLevel = "insane";
+  else if (score >= 20) currentLevel = "hard";
+  else if (score >= 10) currentLevel = "medium";
+  else currentLevel = "easy";
 
-	if (score===-1) {
-		scoreDisplay.innerHTML = 0;
-	}
-	else{
-        scoreDisplay.innerHTML = score;
-	}
+  time = levels[currentLevel].time;
+  seconds.innerHTML = time;
 }
 
-function matchWords(){
-	if (wordInput.value === currentWord.innerHTML) {
-            message.innerHTML = 'correct!!!';
-            return true;
-		}
-		else {
-			message.innerHTML = '';
-			return false;
-		}
+function startMatch() {
+  if (matchWords()) {
+    isPlaying = true;
+
+    updateLevel(); // 🔥 update difficulty
+    time = levels[currentLevel].time + 1;
+
+    showWord();
+    wordInput.value = '';
+    score++;
+  }
+
+  scoreDisplay.innerHTML = score < 0 ? 0 : score;
 }
 
-function showWord(words) {
+function matchWords() {
+  if (wordInput.value === currentWord.innerHTML) {
+    message.innerHTML = 'Correct!';
+    return true;
+  } else {
+    message.innerHTML = '';
+    return false;
+  }
+}
+
+function showWord() {
+  const words = levels[currentLevel].words;
   const randIndex = Math.floor(Math.random() * words.length);
   currentWord.innerHTML = words[randIndex];
-}	
-
-function countdown(){
-	if(time>0){
-       time--;
-	}
-	else if(time===0){
-		isPlaying = false;
-	}
-	timeDisplay.innerHTML = time;
 }
 
-function checkStatus(){
-	if (!isPlaying && time===0) {
-		message.innerHTML = 'game Over!!!';
-		score = -1;
-	}
+function countdown() {
+  if (time > 0) {
+    time--;
+  } else {
+    isPlaying = false;
+  }
+  timeDisplay.innerHTML = time;
+}
+
+function checkStatus() {
+  if (!isPlaying && time === 0) {
+    message.innerHTML = 'Game Over!';
+    score = 0;
+  }
 }
